77bb735f0351a03dce488a1149b7aea0fa083a84
Irving Micah <imicah@si-i4.kzn.21-school.ru>
2020-10-20 16:50:52 +0300

    refactor iterator list

--- list/Iterator_list.hpp
+++ list/Iterator_list.hpp
@@ -6,7 +6,7 @@
 /*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 12:39:34 by imicah            #+#    #+#             */
-/*   Updated: 2020/10/20 15:56:54 by imicah           ###   ########.fr       */
+/*   Updated: 2020/10/20 16:49:13 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,54 +24,29 @@ private:
 	T*		_ptr;
 
 public:
-	IteratorList(T *p = 0);
-	IteratorList(const IteratorList<T> &iterator);
-	~IteratorList();
+	IteratorList(T *p = 0) : _ptr(p) { };
+	IteratorList(const IteratorList<T> &it) : _ptr(it._ptr) { };
+	~IteratorList() { };
 
-	IteratorList<T>		&operator=(const IteratorList<T> &);
+	IteratorList<T>		&operator=(const IteratorList<T> &it) {
+		if (this != &it)
+			_ptr = it._ptr;
+		return (*this);
+	};
 
-	bool				operator!=(const IteratorList<T> &) const;
-	bool				operator==(const IteratorList<T> &) const;
-	IteratorList<T>		&operator++();
-	IteratorList<T>		&operator--();
-	T					&operator*();
+	bool				operator!=(const IteratorList<T> &it) const { return (_ptr != it._ptr); };
+	bool				operator==(const IteratorList<T> &it) const { return (_ptr == it._ptr); };
+	T					&operator*() { return (_ptr->value); };
+
+	IteratorList<T>		&operator++() {
+		_ptr = _ptr->next;
+		return (*this);
+	};
+
+	IteratorList<T>		&operator--() {
+		_ptr = _ptr->prev;
+		return (*this);
+	};
 };
-
-template <class T>
-IteratorList<T>::IteratorList(T *p) : _ptr(p) { }
-
-template <class T>
-IteratorList<T>::IteratorList(const IteratorList<T> &iterator) : _ptr(iterator._ptr) { }
-
-template <class T>
-IteratorList<T>::~IteratorList() { }
-
-template<class T>
-IteratorList<T>		&IteratorList<T>::operator=(const IteratorList<T> &iterator) {
-	if (this != &iterator)
-		_ptr = iterator._ptr;
-	return (*this);
-}
-
-template<class T>
-bool	IteratorList<T>::operator==(const IteratorList<T> &iterator) const { return (_ptr == iterator._ptr); }
-
-template<class T>
-bool	IteratorList<T>::operator!=(const IteratorList<T> &iterator) const { return (_ptr != iterator._ptr); }
-
-template<class T>
-IteratorList<T>		&IteratorList<T>::operator++() {
-	_ptr = _ptr->next;
-	return (*this);
-}
-
-template<class T>
-IteratorList<T>		&IteratorList<T>::operator--() {
-	_ptr = _ptr->prev;
-	return (*this);
-}
-
-template<class T>
-T	&IteratorList<T>::operator*() { return (_ptr->value); }
 
 #endif
