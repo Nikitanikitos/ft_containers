e4db744264cfe2755de339135c2f7c646eb87740
Irving Micah <imicah@si-i4.kzn.21-school.ru>
2020-10-21 21:00:45 +0300

    added tests for constructors list

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 15:40:41 by imicah            #+#    #+#             */
-/*   Updated: 2020/10/21 20:31:06 by imicah           ###   ########.fr       */
+/*   Updated: 2020/10/21 20:58:28 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,13 +67,14 @@ TEST_CASE("Copy constructor list container", "[list]") {
 		for (int i = 0; i < 10; i++) {
 			ft::list<int>		ft_list_for_copy(i);
 			std::list<int>		list_for_copy(i);
-
 			ft::list<int>		ft_list(ft_list_for_copy);
 			std::list<int>		list(list_for_copy);
 
 			REQUIRE(list.empty() == ft_list.empty());
 			REQUIRE(list.size() == ft_list.size());
+
 			list.clear();
+			list_for_copy.clear();
 			// TODO added ft_list.clear()
 		}
 	}
@@ -91,10 +92,56 @@ TEST_CASE("Copy constructor list container", "[list]") {
 			REQUIRE(list.size() == ft_list.size());
 			REQUIRE(*list.begin() == *ft_list.begin());
 			list.clear();
+			list_for_copy.clear();
+			// TODO added ft_list.clear()
+		}
+	}
+}
+
+TEST_CASE("Assignation operator list container", "[list]") {
+
+	SECTION("AssignationOperator_CreateEmptylist_MatchWithOriginal") {
+		ft::list<int>		ft_list_for_copy;
+		std::list<int>		list_for_copy;
+		ft::list<int>		ft_list = ft_list_for_copy;
+		std::list<int>		list = list_for_copy;
+
+		REQUIRE(list.empty() == ft_list.empty());
+		REQUIRE(list.size() == ft_list.size());
+	}
+
+	SECTION("AssignationOperator_CreateWithSize_MatchWithOriginal") {
+		for (int i = 0; i < 20; i++) {
+			ft::list<int>		ft_list_for_copy(i);
+			std::list<int>		list_for_copy(i);
+
+			ft::list<int>		ft_list = ft_list_for_copy;
+			std::list<int>		list = list_for_copy;
+
+			REQUIRE(list.empty() == ft_list.empty());
+			REQUIRE(list.size() == ft_list.size());
+			list.clear();
+			list_for_copy.clear();
 			// TODO added ft_list.clear()
 		}
 	}
 
+	SECTION("AssignationOperator_CreateWithSizeAndDefaultVal_MatchWithOriginal") {
+		for (int i = 0; i < 20; i++) {
+			ft::list<char>		ft_list_for_copy(i, 'a' + i);
+			std::list<char>		list_for_copy(i, 'a' + i);
+
+			ft::list<char>		ft_list = ft_list_for_copy;
+			std::list<char>		list = list_for_copy;
+
+			*ft_list.begin();
+			REQUIRE(list.empty() == ft_list.empty());
+			REQUIRE(list.size() == ft_list.size());
+			REQUIRE(*list.begin() == *ft_list.begin());
+			list.clear();
+			// TODO added ft_list.clear()
+		}
+	}
 }
 
 //int 	main() {
