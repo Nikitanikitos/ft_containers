8e12ebfbd06d2ef8705872395c13059c35af38c1
Irving Micah <imicah@si-i4.kzn.21-school.ru>
2020-10-22 14:46:46 +0300

    added WithManyNode test in list

--- list/tests/list_iterator_tests.cpp
+++ list/tests/list_iterator_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 13:22:29 by imicah            #+#    #+#             */
-/*   Updated: 2020/10/22 14:11:03 by imicah           ###   ########.fr       */
+/*   Updated: 2020/10/22 14:17:07 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,26 @@
 #include <list>
 #include "catch.hpp"
 
-TEMPLATE_TEST_CASE("Default iterator", "[list] [iterator]", int, char) {
+TEMPLATE_TEST_CASE_SIG("Default iterator", "[list] [iterator]",
+										((typename T, int V), T, V), (int, 10), (char, 20)) {
 
-	ft::list<TestType> ft_list;
-	std::list<TestType> list;
+	ft::list<T> ft_list;
+	std::list<T> list;
 
 	SECTION("InEmptyList") {
-		typename ft::list<TestType>::iterator	ft_it = ft_list.begin();
-		typename std::list<TestType>::iterator	it = list.begin();
+		typename ft::list<T>::iterator	ft_it = ft_list.begin();
+		typename std::list<T>::iterator	it = list.begin();
 
 		REQUIRE(ft_it == ft_list.end());
 		REQUIRE(it == list.end());
 	}
 
-	SECTION("WithOneElement") {
+	SECTION("WithOneNode") {
 		ft_list.push_back(1);
 		list.push_back(1);
 
-		typename ft::list<TestType>::iterator	ft_it = ft_list.begin();
-		typename std::list<TestType>::iterator	it = list.begin();
+		typename ft::list<T>::iterator	ft_it = ft_list.begin();
+		typename std::list<T>::iterator	it = list.begin();
 		int			ft_count_iterations;
 		int			count_iterations;
 
@@ -47,4 +48,25 @@ TEMPLATE_TEST_CASE("Default iterator", "[list] [iterator]", int, char) {
 		REQUIRE(ft_count_iterations == count_iterations);
 	}
 
+	SECTION("WithManyNode") {
+		for (int i = 0; i < V; ++i) {
+			ft_list.push_back(i);
+			list.push_back(i);
+		}
+
+		typename ft::list<T>::iterator	ft_it = ft_list.begin();
+		typename std::list<T>::iterator	it = list.begin();
+		int			ft_count_iterations;
+		int			count_iterations;
+
+		REQUIRE(ft_it != ft_list.end());
+		REQUIRE(it != list.end());
+		REQUIRE(*ft_it == *it);
+
+		for (ft_count_iterations = 0; ft_it != ft_list.end(); ++ft_count_iterations)
+			++ft_it;
+		for (count_iterations = 0; it != list.end(); ++count_iterations)
+			++it;
+		REQUIRE(ft_count_iterations == count_iterations);
+	}
 }
