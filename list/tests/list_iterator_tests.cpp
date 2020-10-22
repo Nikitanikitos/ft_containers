c49617ae486f5f58b76bef645980c32404e96e59
Irving Micah <imicah@si-i4.kzn.21-school.ru>
2020-10-22 20:46:11 +0300

    add reverse iterator test

--- list/tests/list_iterator_tests.cpp
+++ list/tests/list_iterator_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 13:22:29 by imicah            #+#    #+#             */
-/*   Updated: 2020/10/22 20:23:19 by imicah           ###   ########.fr       */
+/*   Updated: 2020/10/22 20:43:39 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,17 @@ TEMPLATE_TEST_CASE_SIG("Default iterator", "[list] [iterator]",
 
 	SECTION("InEmptyList") {
 		ft_it = ft_list.begin();
-		it = list.begin();
 
 		REQUIRE(ft_it == ft_list.end());
-//		REQUIRE(it == list.end());
 	}
 
 	SECTION("WithOneNode") {
 		ft_list.push_back(1);
 		list.push_back(1);
 		ft_it = ft_list.begin();
-		it = list.begin();
 
 		REQUIRE(ft_it != ft_list.end());
-//		REQUIRE(it != list.end());
-		REQUIRE(*ft_it == *it);
+		REQUIRE(*ft_it == *list.begin());
 	}
 
 	SECTION("WithManyNode") {
@@ -49,52 +45,64 @@ TEMPLATE_TEST_CASE_SIG("Default iterator", "[list] [iterator]",
 			ft_list.push_back(i);
 			list.push_back(i);
 		}
-		ft_it = ft_list.begin();
-		it = list.begin();
 
-		REQUIRE(ft_it != ft_list.end());
-//		REQUIRE(it != list.end());
+		SECTION("Increment iterator") {
+			ft_it = ft_list.begin();
+			it = list.begin();
 
-		while (ft_it != ft_list.end() && it != list.end()) {
-			if (ft_it != ft_list.end())
-				++ft_count_iterations;
-			if (it != list.end())
-				++count_iterations;
-			REQUIRE(*ft_it == *it);
-			++ft_it;
-			++it;
+			while (ft_it != ft_list.end() && it != list.end()) {
+				if (ft_it != ft_list.end())
+					++ft_count_iterations;
+				if (it != list.end())
+					++count_iterations;
+				REQUIRE(*ft_it == *it);
+				++ft_it;
+				++it;
+			}
+
+			REQUIRE(ft_count_iterations == count_iterations);
 		}
-		REQUIRE(ft_count_iterations == count_iterations);
+
+		SECTION("Decrement test") {
+			ft_it = ft_list.end();
+			it = list.end();
+
+			while (ft_it != ft_list.begin() && it != list.begin()) {
+				if (ft_it != ft_list.begin())
+					++ft_count_iterations;
+				if (it != list.begin())
+					++count_iterations;
+				--ft_it;
+				--it;
+				REQUIRE(*ft_it == *it);
+			}
+
+			REQUIRE(ft_count_iterations == count_iterations);
+		}
 	}
 }
 
 TEMPLATE_TEST_CASE_SIG("Default constant iterator", "[list] [iterator]",
 							   ((typename T, int V), T, V), (int, 10), (char, 20)) {
 
-	ft::list<T> ft_list;
-	std::list<T> list;
+	ft::list<T>								ft_list;
+	std::list<T>							list;
 	typename ft::list<T>::const_iterator	ft_it;
 	typename std::list<T>::const_iterator	it;
 
 	SECTION("InEmptyList") {
 		ft_it = ft_list.begin();
-		it = list.begin();
 
 		REQUIRE(ft_it == ft_list.end());
-//		REQUIRE(it == list.end());
 	}
 
 	SECTION("WithOneNode") {
-		int			ft_count_iterations;
-		int			count_iterations;
 		ft_list.push_back(1);
 		list.push_back(1);
 		ft_it = ft_list.begin();
-		it = list.begin();
 
 		REQUIRE(ft_it != ft_list.end());
-//		REQUIRE(it != list.end());
-		REQUIRE(*ft_it == *it);
+		REQUIRE(*ft_it == *list.begin());
 	}
 
 	SECTION("WithManyNode") {
@@ -105,50 +113,64 @@ TEMPLATE_TEST_CASE_SIG("Default constant iterator", "[list] [iterator]",
 			ft_list.push_back(i);
 			list.push_back(i);
 		}
-		ft_it = ft_list.begin();
-		it = list.begin();
 
-		REQUIRE(ft_it != ft_list.end());
-//		REQUIRE(it != list.end());
+		SECTION("Increment iterator") {
+			ft_it = ft_list.begin();
+			it = list.begin();
 
-		while (ft_it != ft_list.end() && it != list.end()) {
-			if (ft_it != ft_list.end())
-				++ft_count_iterations;
-			if (it != list.end())
-				++count_iterations;
-			REQUIRE(*ft_it == *it);
-			++ft_it;
-			++it;
+			while (ft_it != ft_list.end() && it != list.end()) {
+				if (ft_it != ft_list.end())
+					++ft_count_iterations;
+				if (it != list.end())
+					++count_iterations;
+				REQUIRE(*ft_it == *it);
+				++ft_it;
+				++it;
+			}
+
+			REQUIRE(ft_count_iterations == count_iterations);
 		}
-		REQUIRE(ft_count_iterations == count_iterations);
+
+		SECTION("Decrement test") {
+			ft_it = ft_list.end();
+			it = list.end();
+
+			while (ft_it != ft_list.begin() && it != list.begin()) {
+				if (ft_it != ft_list.begin())
+					++ft_count_iterations;
+				if (it != list.begin())
+					++count_iterations;
+				--ft_it;
+				--it;
+				REQUIRE(*ft_it == *it);
+			}
+
+			REQUIRE(ft_count_iterations == count_iterations);
+		}
 	}
 }
 
 TEMPLATE_TEST_CASE_SIG("Reverse iterator", "[list] [iterator]",
 					   ((typename T, int V), T, V), (int, 10), (char, 20)) {
 
-	ft::list<T>							ft_list;
-	std::list<T>						list;
+	ft::list<T>									ft_list;
+	std::list<T>								list;
 	typename ft::list<T>::reverse_iterator		ft_it;
 	typename std::list<T>::reverse_iterator		it;
 
 	SECTION("InEmptyList") {
 		ft_it = ft_list.rbegin();
-		it = list.rbegin();
 
 		REQUIRE(ft_it == ft_list.rend());
-//		REQUIRE(it == list.end());
 	}
 
 	SECTION("WithOneNode") {
 		ft_list.push_back(1);
 		list.push_back(1);
 		ft_it = ft_list.rbegin();
-		it = list.rbegin();
 
 		REQUIRE(ft_it != ft_list.rend());
-//		REQUIRE(it != list.end());
-		REQUIRE(*ft_it == *it);
+		REQUIRE(*ft_it == *list.rbegin());
 	}
 
 	SECTION("WithManyNode") {
@@ -159,22 +181,40 @@ TEMPLATE_TEST_CASE_SIG("Reverse iterator", "[list] [iterator]",
 			ft_list.push_back(i);
 			list.push_back(i);
 		}
-		ft_it = ft_list.rbegin();
-		it = list.rbegin();
 
-		REQUIRE(ft_it != ft_list.rend());
-//		REQUIRE(it != list.end());
+		SECTION("Increment iterator") {
+			ft_it = ft_list.rbegin();
+			it = list.rbegin();
 
-		while (ft_it != ft_list.rend() && it != list.rend()) {
-			if (ft_it != ft_list.rend())
-				++ft_count_iterations;
-			if (it != list.rend())
-				++count_iterations;
-			REQUIRE(*ft_it == *it);
-			++ft_it;
-			++it;
+			while (ft_it != ft_list.rend() && it != list.rend()) {
+				if (ft_it != ft_list.rend())
+					++ft_count_iterations;
+				if (it != list.rend())
+					++count_iterations;
+				REQUIRE(*ft_it == *it);
+				++ft_it;
+				++it;
+			}
+
+			REQUIRE(ft_count_iterations == count_iterations);
 		}
-		REQUIRE(ft_count_iterations == count_iterations);
+
+		SECTION("Decrement iterator") {
+			ft_it = ft_list.rend();
+			it = list.rend();
+
+			while (ft_it != ft_list.rbegin() && it != list.rbegin()) {
+				if (ft_it != ft_list.rbegin())
+					++ft_count_iterations;
+				if (it != list.rbegin())
+					++count_iterations;
+				--ft_it;
+				--it;
+				REQUIRE(*ft_it == *it);
+			}
+
+			REQUIRE(ft_count_iterations == count_iterations);
+		}
 	}
 }
 
@@ -188,23 +228,17 @@ TEMPLATE_TEST_CASE_SIG("Reverse constant iterator", "[list] [iterator]",
 
 	SECTION("InEmptyList") {
 		ft_it = ft_list.rbegin();
-		it = list.rbegin();
 
 		REQUIRE(ft_it == ft_list.rend());
-//		REQUIRE(it == list.end());
 	}
 
 	SECTION("WithOneNode") {
-		int			ft_count_iterations;
-		int			count_iterations;
 		ft_list.push_back(1);
 		list.push_back(1);
 		ft_it = ft_list.rbegin();
-		it = list.rbegin();
 
 		REQUIRE(ft_it != ft_list.rend());
-//		REQUIRE(it != list.end());
-		REQUIRE(*ft_it == *it);
+		REQUIRE(*ft_it == *list.rbegin());
 	}
 
 	SECTION("WithManyNode") {
@@ -215,21 +249,39 @@ TEMPLATE_TEST_CASE_SIG("Reverse constant iterator", "[list] [iterator]",
 			ft_list.push_back(i);
 			list.push_back(i);
 		}
-		ft_it = ft_list.rbegin();
-		it = list.rbegin();
 
-		REQUIRE(ft_it != ft_list.rend());
-//		REQUIRE(it != list.end());
+		SECTION("Increment iterator") {
+			ft_it = ft_list.rbegin();
+			it = list.rbegin();
 
-		while (ft_it != ft_list.rend() && it != list.rend()) {
-			if (ft_it != ft_list.rend())
-				++ft_count_iterations;
-			if (it != list.rend())
-				++count_iterations;
-			REQUIRE(*ft_it == *it);
-			++ft_it;
-			++it;
+			while (ft_it != ft_list.rend() && it != list.rend()) {
+				if (ft_it != ft_list.rend())
+					++ft_count_iterations;
+				if (it != list.rend())
+					++count_iterations;
+				REQUIRE(*ft_it == *it);
+				++ft_it;
+				++it;
+			}
+
+			REQUIRE(ft_count_iterations == count_iterations);
 		}
-		REQUIRE(ft_count_iterations == count_iterations);
+
+		SECTION("Decrement iterator") {
+			ft_it = ft_list.rend();
+			it = list.rend();
+
+			while (ft_it != ft_list.rbegin() && it != list.rbegin()) {
+				if (ft_it != ft_list.rbegin())
+					++ft_count_iterations;
+				if (it != list.rbegin())
+					++count_iterations;
+				--ft_it;
+				--it;
+				REQUIRE(*ft_it == *it);
+			}
+
+			REQUIRE(ft_count_iterations == count_iterations);
+		}
 	}
 }
