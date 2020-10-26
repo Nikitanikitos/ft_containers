9d19b7eea51de3e28c898b83c30ed75c0b37f7f9
Irving Micah <imicah@et-a6.kzn.21-school.ru>
2020-10-26 18:01:09 +0300

    complited iterators

--- vector/iterators/reverse_random_access_iterator.hpp
+++ vector/iterators/reverse_random_access_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 17:21:42 by imicah            #+#    #+#             */
-/*   Updated: 2020/10/26 17:24:03 by imicah           ###   ########.fr       */
+/*   Updated: 2020/10/26 17:59:20 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,34 +27,58 @@ namespace ft
 		T*		_ptr;
 
 	public:
-		rev_random_access_iterator();
-		rev_random_access_iterator(const rev_random_access_iterator<T> &);
-		~rev_random_access_iterator();
+		rev_random_access_iterator() : _ptr(0) { }
+		rev_random_access_iterator(const rev_random_access_iterator<T> &x) : _ptr(x._ptr) { }
+		~rev_random_access_iterator() { }
 
-		rev_random_access_iterator<T>	&operator=(const rev_random_access_iterator<T> &);
+		rev_random_access_iterator<T>	&operator=(const rev_random_access_iterator<T> &x) {
+			if (this != &x)
+				_ptr = x._ptr;
+			return (*this);
+		}
 
-		bool	operator==(const rev_random_access_iterator<T> &);
-		bool	operator!=(const rev_random_access_iterator<T> &);
-		bool	operator<(const rev_random_access_iterator<T> &);
-		bool	operator<=(const rev_random_access_iterator<T> &);
-		bool	operator>(const rev_random_access_iterator<T> &);
-		bool	operator>=(const rev_random_access_iterator<T> &);
+		bool	operator==(const rev_random_access_iterator<T> &x) { return (*_ptr == *x._ptr); }
+		bool	operator!=(const rev_random_access_iterator<T> &x) { return (*_ptr != *x._ptr); }
+		bool	operator<(const rev_random_access_iterator<T> &x) { return (*_ptr < *x._ptr); }
+		bool	operator<=(const rev_random_access_iterator<T> &x) { return (*_ptr <= *x._ptr); }
+		bool	operator>(const rev_random_access_iterator<T> &x) { return (*_ptr > *x._ptr); }
+		bool	operator>=(const rev_random_access_iterator<T> &x) { return (*_ptr >= *x._ptr); }
 
-		rev_random_access_iterator<T>	&operator++();
-		rev_random_access_iterator<T>	&operator--();
+		rev_random_access_iterator<T>	&operator++() { _ptr--; return (*this); }
+		rev_random_access_iterator<T>	&operator--() { _ptr++; return (*this); }
 
-		rev_random_access_iterator<T>	operator++(int);
-		rev_random_access_iterator<T>	operator--(int);
+		rev_random_access_iterator<T>	operator++(int) {
+			rev_random_access_iterator<T>		temp(_ptr);
 
-		rev_random_access_iterator<T>	operator+(rev_random_access_iterator<T> &);
-		rev_random_access_iterator<T>	operator-(rev_random_access_iterator<T> &);
+			--_ptr;
+			return (temp);
+		}
 
-		rev_random_access_iterator<T>	operator+=(rev_random_access_iterator<T> &);
-		rev_random_access_iterator<T>	operator-=(rev_random_access_iterator<T> &);
-		rev_random_access_iterator<T>	operator[](size_t);
+		rev_random_access_iterator<T>	operator--(int) {
+			rev_random_access_iterator<T>		temp(_ptr);
 
-		T		&operator*();
-		T		*operator->();
+			++_ptr;
+			return (temp);
+		}
+
+		rev_random_access_iterator<T>	operator+(rev_random_access_iterator<T> &x)
+																	{ return (_ptr + x._ptr); }
+		rev_random_access_iterator<T>	operator-(rev_random_access_iterator<T> &x)
+																	{ return (_ptr + x._ptr); }
+		rev_random_access_iterator<T>	operator+=(rev_random_access_iterator<T> &x) {
+			_ptr += x._ptr;
+			return (*this);
+		}
+
+		rev_random_access_iterator<T>	operator-=(rev_random_access_iterator<T> &x) {
+			_ptr -= x._ptr;
+			return (*this);
+		}
+
+		rev_random_access_iterator<T>	operator[](size_t n) { return (_ptr[n]); }
+
+		T		&operator*() { return (*_ptr); }
+		T		*operator->() { return (_ptr); }
 
 		T*		_get_ptr() const { return(_ptr); } // TODO придумать что нибудь по лучше
 
@@ -67,36 +91,76 @@ namespace ft
 		T*		_ptr;
 
 	public:
-		const_rev_random_access_iterator();
-		const_rev_random_access_iterator(const const_rev_random_access_iterator<T> &);
-		const_rev_random_access_iterator(const rev_random_access_iterator<T> &);
-		~const_rev_random_access_iterator();
+		const_rev_random_access_iterator() : _ptr(0) { }
+		const_rev_random_access_iterator(const const_rev_random_access_iterator<T> &x)
+																			: _ptr(x._ptr) { }
+		const_rev_random_access_iterator(const rev_random_access_iterator<T> &x)
+																			: _ptr(x._get_ptr()) { }
+		~const_rev_random_access_iterator() { }
 
-		const_rev_random_access_iterator<T>	&operator=(const const_rev_random_access_iterator<T> &);
-		const_rev_random_access_iterator<T>	&operator=(const rev_random_access_iterator<T> &);
+		const_rev_random_access_iterator<T>	&operator=(
+													const const_rev_random_access_iterator<T> &x) {
+			if (this != &x)
+				_ptr = x._ptr;
+			return (*this);
+		}
 
-		bool	operator==(const const_rev_random_access_iterator<T> &);
-		bool	operator!=(const const_rev_random_access_iterator<T> &);
-		bool	operator<(const const_rev_random_access_iterator<T> &);
-		bool	operator<=(const const_rev_random_access_iterator<T> &);
-		bool	operator>(const const_rev_random_access_iterator<T> &);
-		bool	operator>=(const const_rev_random_access_iterator<T> &);
+		const_rev_random_access_iterator<T>	&operator=(const rev_random_access_iterator<T> &x) {
+			if (this != &x)
+			_ptr = x._get_ptr();
+			return (*this);
+		}
 
-		const_rev_random_access_iterator<T>	&operator++();
-		const_rev_random_access_iterator<T>	&operator--();
+		bool	operator==(const const_rev_random_access_iterator<T> &x)
+																	{ return (*_ptr == *x._ptr); }
+		bool	operator!=(const const_rev_random_access_iterator<T> &x)
+																	{ return (*_ptr == *x._ptr); }
+		bool	operator<(const const_rev_random_access_iterator<T> &x)
+																	{ return (*_ptr == *x._ptr); }
+		bool	operator<=(const const_rev_random_access_iterator<T> &x)
+																	{ return (*_ptr == *x._ptr); }
+		bool	operator>(const const_rev_random_access_iterator<T> &x)
+																	{ return (*_ptr == *x._ptr); }
+		bool	operator>=(const const_rev_random_access_iterator<T> &x)
+																	{ return (*_ptr == *x._ptr); }
 
-		const_rev_random_access_iterator<T>	operator++(int);
-		const_rev_random_access_iterator<T>	operator--(int);
+		const_rev_random_access_iterator<T>	&operator++() { _ptr--; return (*this); }
+		const_rev_random_access_iterator<T>	&operator--() { _ptr++; return (*this); }
 
-		const_rev_random_access_iterator<T>	operator+(const_rev_random_access_iterator<T> &);
-		const_rev_random_access_iterator<T>	operator-(const_rev_random_access_iterator<T> &);
+		const_rev_random_access_iterator<T>	operator++(int) {
+			rev_random_access_iterator<T>		temp(_ptr);
 
-		const_rev_random_access_iterator<T>	operator+=(const_rev_random_access_iterator<T> &);
-		const_rev_random_access_iterator<T>	operator-=(const_rev_random_access_iterator<T> &);
-		const T								operator[](size_t);
+			--_ptr;
+			return (temp);
+		}
 
-		T		&operator*();
-		T		*operator->();
+		const_rev_random_access_iterator<T>	operator--(int) {
+			rev_random_access_iterator<T>		temp(_ptr);
+
+			++_ptr;
+			return (temp);
+		}
+
+		const_rev_random_access_iterator<T>	operator+(const_rev_random_access_iterator<T> &x)
+																		{ return (_ptr + x._ptr); }
+
+		const_rev_random_access_iterator<T>	operator-(const_rev_random_access_iterator<T> &x)
+																		{ return (_ptr - x._ptr); }
+
+		const_rev_random_access_iterator<T>	operator+=(const_rev_random_access_iterator<T> &x) {
+			_ptr += x._ptr;
+			return (*this);
+		}
+
+		const_rev_random_access_iterator<T>	operator-=(const_rev_random_access_iterator<T> &x) {
+			_ptr -= x._ptr;
+			return (*this);
+		}
+
+		const T								operator[](size_t n) { return (_ptr[n]); }
+
+		T									&operator*() { return (*_ptr); }
+		T									*operator->() { return (_ptr); }
 
 		T*		_get_ptr() const { return(_ptr); } // TODO придумать что нибудь по лучше
 
