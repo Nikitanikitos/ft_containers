b96729e3971dc945d8451eb1b8e54935cee9a185
Irving Micah <imicah@si-i4.kzn.21-school.ru>
2020-11-01 15:21:16 +0300

    change main

--- main.cpp
+++ main.cpp
@@ -6,25 +6,9 @@
 /*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 23:31:40 by imicah            #+#    #+#             */
-/*   Updated: 2020/10/30 20:09:43 by imicah           ###   ########.fr       */
+/*   Updated: 2020/11/01 13:43:11 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-//#include "catch.hpp"
-#include "map.hpp"
-#include <map>
-
-int 	main() {
-	ft::map<int, int>	map;
-
-	map.insert(std::make_pair(13, 42));
-	map.insert(std::make_pair(6, 42));
-	map.insert(std::make_pair(2, 42));
-	map.insert(std::make_pair(42, 42));
-	map.insert(std::make_pair(21, 42));
-	map.insert(std::make_pair(42, 42));
-	map.insert(std::make_pair(21, 42));
-
-	return (0);
-}
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
