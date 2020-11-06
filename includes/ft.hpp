188917546591ce5934b9d5734e9364c944f4078a
Nikita <nikitos_athletos@mail.ru>
2020-11-06 18:32:38 +0300

    refactor ft.hpp

--- includes/ft.hpp
+++ includes/ft.hpp
@@ -6,12 +6,15 @@
 /*   By: nikita <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 23:33:02 by imicah            #+#    #+#             */
-/*   Updated: 2020/11/05 14:04:02 by nikita           ###   ########.fr       */
+/*   Updated: 2020/11/06 18:05:18 by nikita           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_CONTAINERS_FT_HPP
 # define FT_CONTAINERS_FT_HPP
+
+# include <iostream>
+# include <string>
 
 namespace ft {
 	template<bool B, class T = void>
@@ -22,8 +25,18 @@ namespace ft {
 
 	template < class Key, class Value, class Compare = std::less<Key>, class Alloc = std::allocator<std::pair<const Key,Value> > >
 	class	map;
+
 	template<class T, class Alloc = std::allocator<T> >
 	class	list;
+
+	template <class T, class Container = list<T> >
+	class	queue;
+
+	template <class T, class Container = list<T> >
+	class	stack;
+
+	template <class T, class Alloc = std::allocator<T> >
+	class	vector;
 
 	template < class Key, class Value, class Compare = std::less<Key>, class Alloc = std::allocator<std::pair<const Key,Value> > >
 	class	tree;
@@ -38,6 +51,11 @@ namespace ft {
 	template<class T, class U>
 	class	const_list_iterator;
 
+	template<class T>
+	class	vector_iterator;
+	template<class T>
+	class	const_vector_iterator;
+
 	template<class T, class U>
 	class	reverse_map_iterator;
 	template<class T, class U>
@@ -47,6 +65,11 @@ namespace ft {
 	class	reverse_list_iterator;
 	template<class T, class U>
 	class	const_reverse_list_iterator;
+
+	template<class T>
+	class	reverse_vector_iterator;
+	template<class T>
+	class	const_reverse_vector_iterator;
 }
 
 #endif
